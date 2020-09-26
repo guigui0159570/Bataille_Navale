c39fcafa45ca8ad7319ca45c0a763b3bfe55a4aa
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-09-26 14:49:15 +0200

    Ajout du programme

---
+++ Jeu.py
@@ -0,0 +1,12 @@
+def choix():
+    lettres = ["a","b","c","d","e","f","g","h","i","j","A","B","C","D","E","F","G","H","I","J"]
+    chiffres = ["0","1","2","3","4","5","6","7","8","9"]
+    while True:
+        choix = input("Quelles sont les coordonnées de la frappe : ")
+        if len(choix) == 2:
+            ele1 = choix[0]
+            ele2 = choix[1]
+            if ele1 in lettres:
+                if ele2 in chiffres:
+                    break
+

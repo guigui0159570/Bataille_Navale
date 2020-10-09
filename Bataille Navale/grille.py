93b7a41d92535efbdf1305a34e01808ab9700cd5
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-10-09 14:40:18 +0200

    ADD grille

---
+++ Bataille Navale/grille.py
@@ -0,0 +1,20 @@
+if lettre == "A":
+            return self.A[chiffre]
+        elif lettre == "B":
+            return self.B[chiffre]
+        elif lettre == "C":
+            return self.C[chiffre]
+        elif lettre == "D":
+            return self.D[chiffre]
+        elif lettre == "E":
+            return self.E[chiffre]
+        elif lettre == "F":
+            return self.F[chiffre]
+        elif lettre == "G":
+            return self.G[chiffre]
+        elif lettre == "H":
+            return self.H[chiffre]
+        elif lettre == "I":
+            return self.I[chiffre]
+        elif lettre == "J":
+            return self.J[chiffre]

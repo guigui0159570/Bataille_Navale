7d88b3ea234024efe7af17ff668c9d076c7653a1
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-10-09 15:04:31 +0200

    Update Jeu.py

--- Bataille Navale/Jeu.py
+++ Bataille Navale/Jeu.py
@@ -1,4 +1,6 @@
-def choix():
+from grille import Grille
+
+def choix(grille):
     lettres = ["a","b","c","d","e","f","g","h","i","j","A","B","C","D","E","F","G","H","I","J"]
     chiffres = ["0","1","2","3","4","5","6","7","8","9"]
     while True:
@@ -8,5 +10,10 @@ def choix():
             ele2 = choix[1]
             if ele1 in lettres:
                 if ele2 in chiffres:
-                    break
+                    lettre = ele1
+                    chiffre = ele2
+                    print(grille.affiche(lettre, int(chiffre)))
 
+grille1 = Grille()
+grille2 = Grille()
+choix(grille1)

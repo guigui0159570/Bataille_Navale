12a34752e4faee9d465db2810b577f5499bb4e31
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-11-01 21:00:59 +0100

    Update grille.py

--- grille.py
+++ grille.py
@@ -1,57 +1,26 @@
 class Grille():
+    """
+    Classe qui générère des grilles.
+    Un objet grille contient plusieurs listes correspondant à chaque ligne de la bataille navale.
+    """
     def __init__(self):
-        self.A = [None, None, None, None, None, None, None, None, None, None]
-        self.B = [None, None, None, None, None, None, None, None, None, None]
-        self.C = [None, None, None, None, None, None, None, None, None, None]
-        self.D = [None, None, None, None, None, None, None, None, None, None]
-        self.E = [None, None, None, None, None, None, None, None, None, None]
-        self.F = [None, None, None, None, None, None, None, None, None, None]
-        self.G = [None, None, None, None, None, None, None, None, None, None]
-        self.H = [None, None, None, None, None, None, None, None, None, None]
-        self.I = [None, None, None, None, None, None, None, None, None, None]
-        self.J = [None, None, None, None, None, None, None, None, None, None]
+        self.A = [None] * 10
+        self.B = [None] * 10
+        self.C = [None] * 10
+        self.D = [None] * 10
+        self.E = [None] * 10
+        self.F = [None] * 10
+        self.G = [None] * 10
+        self.H = [None] * 10
+        self.I = [None] * 10
+        self.J = [None] * 10
+        self.dict = {"A":self.A, "B":self.B, "C":self.C, "D":self.D, "E":self.E, "F":self.F, "G":self.G, "H":self.H, "I":self.H, "J":self.J}
         
-    def affiche(self, lettre, chiffre):
-        L = [0,1,2,3,4,5,6,7,8,9]
-        if lettre == "A" or lettre == "a":
-            return self.A[chiffre]
-        elif lettre == "B" or lettre == "b":
-            return self.B[chiffre]
-        elif lettre == "C" or lettre == "c":
-            return self.C[chiffre]
-        elif lettre == "D" or lettre == "d" :
-            return self.D[chiffre]
-        elif lettre == "E" or lettre == "e":
-            return self.E[chiffre]
-        elif lettre == "F" or lettre == "f":
-            return self.F[chiffre]
-        elif lettre == "G" or lettre == "g":
-            return self.G[chiffre]
-        elif lettre == "H" or lettre == "h":
-            return self.H[chiffre]
-        elif lettre == "I" or lettre == "i":
-            return self.I[chiffre]
-        elif lettre == "J" or lettre == "j":
-            return self.J[chiffre]
+    def returng(self, lettre, chiffre):
+        return self.dict[lettre][chiffre]
+    
+    def printg(self, lettre, chiffre):
+        print(self.dict[lettre][chiffre])
         
     def modif(self, lettre, chiffre, état):
-        if lettre == "A" or lettre == "a":
-            self.A[chiffre] = état
-        elif lettre == "B" or lettre == "b":
-            self.B[chiffre] = état
-        elif lettre == "C" or lettre == "c":
-            self.C[chiffre] = état
-        elif lettre == "D" or lettre == "d":
-            self.D[chiffre] = état
-        elif lettre == "E" or lettre == "e":
-            self.E[chiffre] = état
-        elif lettre == "F" or lettre == "f":
-            self.F[chiffre] = état
-        elif lettre == "G" or lettre == "g":
-            self.G[chiffre] = état
-        elif lettre == "H" or lettre == "h":
-            self.H[chiffre] = état
-        elif lettre == "I" or lettre == "i":
-            self.I[chiffre] = état
-        elif lettre == "J" or lettre == "j":
-            self.J[chiffre] = état
+        self.dict[lettre][chiffre] = état

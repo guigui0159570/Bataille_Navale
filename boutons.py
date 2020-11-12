dcb9ab23509f683d40c62326d431a898b9696acc
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-11-12 20:45:51 +0100

    Version finale - flotte.py

--- boutons.py
+++ boutons.py
@@ -2,9 +2,12 @@ import pygame
 from pygame.locals import*
 
 noir = (0, 0, 0)
-''' ici nous avons crée les boutons il nous reste juste a les integrer
+
 class Boutons():
     def __init__(self, color, x,y,width,height, text='', colortext=noir):
+        """
+        Méthode constructeur qui créer le bouton.
+        """
         self.color = color
         self.x = x
         self.y = y
@@ -14,7 +17,9 @@ class Boutons():
         self.colortext = colortext
 
     def draw(self,win,outline=None):
-        #Call this method to draw the button on the screen
+        """
+        Méthode qui dessine le bouton.
+        """
         if outline:
             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
             
@@ -26,6 +31,9 @@ class Boutons():
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self, pos):
+        """
+        Méthode qui renvoie True ou False si on clique dessus.
+        """
         #Pos is the mouse position or a tuple of (x,y) coordinates
         if pos[0] > self.x and pos[0] < self.x + self.width:
             if pos[1] > self.y and pos[1] < self.y + self.height:
